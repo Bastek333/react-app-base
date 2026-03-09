57fc76d594e0129ac39937dae5ab6903aee53440
Krzysztof Bednarski <kbedn@o2.pl>
2026-03-09 13:22:26 +0100

    code examples

--- src/api/dummyData.ts
+++ src/api/dummyData.ts
@@ -1,14 +1,20 @@
-export interface DummyDataInterface {userId: number, id: number, title: string, completed: boolean};
+export interface DummyDataInterface {
+  userId: number;
+  id: number;
+  title: string;
+  completed: boolean;
+}
 
 export const dummyData = async (): Promise<DummyDataInterface[]> => {
-  return fetch('https://jsonplaceholder.typicode.com/todos').then(res => {
-    if (res.ok) {
-      return res.json();
-    }
-    throw new Error('Network response was not ok.');
-  }).catch(error => {
-    console.log(error.message);
-    return [] as DummyDataInterface[];
-  })
-}
-  
+  return fetch('https://jsonplaceholder.typicode.com/todos')
+    .then((res) => {
+      if (res.ok) {
+        return res.json();
+      }
+      throw new Error('Network response was not ok.');
+    })
+    .catch((error) => {
+      console.log(error.message);
+      return [] as DummyDataInterface[];
+    });
+};
--- src/components/DummyComponent/DummyComponent.tsx
+++ src/components/DummyComponent/DummyComponent.tsx
@@ -12,30 +12,36 @@ interface DataFromFetch {
   completed: boolean;
 }
 
-const DummyComponent: React.FC<DummyComponentProps> = ({oneprop, twoprop}) => {
+const DummyComponent: React.FC<DummyComponentProps> = ({
+  oneprop,
+  twoprop,
+}) => {
   const [data, setData] = useState<DataFromFetch>();
-  
+
   useEffect(() => {
     const getData = async () => {
-      await fetch('https://jsonplaceholder.typicode.com/todos/1').then(res => {
-        if (res.ok) {
-          res.json().then(data => setData(data));
-        } else {
-          throw new Error('Network failure');
-        }
-      })
-      .catch(error => console.log(error.message));
-    }
+      await fetch('https://jsonplaceholder.typicode.com/todos/1')
+        .then((res) => {
+          if (res.ok) {
+            res.json().then((data) => setData(data));
+          } else {
+            throw new Error('Network failure');
+          }
+        })
+        .catch((error) => console.log(error.message));
+    };
 
     getData();
-  },[]);
+  }, []);
 
   return (
     <>
-      <div>Dummy component {oneprop} {twoprop}</div>
+      <div>
+        Dummy component {oneprop} {twoprop}
+      </div>
       {data?.userId && <div>User id: {data.userId}</div>}
     </>
-  )
-}
+  );
+};
 
 export default DummyComponent;
