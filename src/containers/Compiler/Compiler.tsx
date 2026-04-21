b01a7c86675102b86a9fb784c72b74e9d61a2158
Krzysztof Bednarski <kbedn@o2.pl>
2026-04-21 08:28:00 +0200

    Compiler and type test updates

--- src/containers/Compiler/Compiler.tsx
+++ src/containers/Compiler/Compiler.tsx
@@ -1,42 +1,45 @@
-import React, { memo, useState } from 'react';
+import React, { memo, useState } from "react";
 
 interface ListTestComponentProps {
-  listItems: {
-    id: number;
-    name: string;
-  }[]
+	listItems: {
+		id: number;
+		name: string;
+	}[];
 }
 
-const ListTestComponent: React.FC<ListTestComponentProps> = ({listItems}) => {
+const ListTestComponent: React.FC<ListTestComponentProps> = ({ listItems }) => {
+	console.log("item render");
 
-  console.log('item render')
-
-  return (
-    <ul>
-    {listItems.map(item => <li key={item.id}>{`Item: ${item.name}`}</li>)}
-    </ul>
-  )
-
-}
+	return (
+		<ul>
+			{listItems.map((item) => (
+				<li key={item.id}>{`Item: ${item.name}`}</li>
+			))}
+		</ul>
+	);
+};
 
 //const MemoListTestComponent = memo(ListTestComponent);
 
 const Compiler = () => {
-  const [listItemsState, setListItemsState] = useState([{id:1, name:'1'}, {id:2, name:'2'}]);
-  const [count, setCount] = useState(0);
+	const [listItemsState, setListItemsState] = useState([
+		{ id: 1, name: "1" },
+		{ id: 2, name: "2" },
+	]);
+	const [count, setCount] = useState(0);
 
-  const handleChange = () => setCount(count + 1);
+	const handleChange = () => setCount(count + 1);
 
-  console.log('parent render')
+	console.log("parent render");
 
-  return (
-    <div>
-      <h1>Compiler</h1>
-      <div onClick={handleChange}>Count {count}</div>
-      <ListTestComponent listItems={listItemsState} />
-      {/* <MemoListTestComponent listItems={listItemsState} /> */}
-    </div>
-  );
-}
+	return (
+		<div>
+			<h1>Compiler</h1>
+			<div onClick={handleChange}>Count {count}</div>
+			<ListTestComponent listItems={listItemsState} />
+			{/* <MemoListTestComponent listItems={listItemsState} /> */}
+		</div>
+	);
+};
 
 export default Compiler;
--- src/typeTest.ts
+++ src/typeTest.ts
@@ -1,16 +1,15 @@
-function unwrapData<T extends {data: any}>(response: T): T extends { data: infer U } ? U : never {
-  return response?.data;
+function unwrapData<T extends { data: any }>(
+	response: T,
+): T extends { data: infer U } ? U : never {
+	return response?.data;
 }
 
-
 const user = unwrapData({
-  data: { id: "1", username: "Alice" },
-  test: "test"
+	data: { id: "1", username: "Alice" },
+	test: "test",
 });
 
-
 console.log(user);
-
 
 type FunctionType<T> = T extends (...arg: any[]) => infer R ? R : never;
 
@@ -18,7 +17,15 @@ type Example = (x: number, y: string) => boolean;
 
 type ReturnFunctionType = FunctionType<Example>;
 
-function exampleFunction<T>(x: T extends infer U ? U : never, y: T extends infer Y ? Y : never): ReturnFunctionType { if (typeof x === "number" && typeof y === "string") { return true; } return false; }
+function exampleFunction<T>(
+	x: T extends infer U ? U : never,
+	y: T extends infer Y ? Y : never,
+): ReturnFunctionType {
+	if (typeof x === "number" && typeof y === "string") {
+		return true;
+	}
+	return false;
+}
 
 console.log(exampleFunction(1, "test"));
 console.log(exampleFunction("test", 1));
